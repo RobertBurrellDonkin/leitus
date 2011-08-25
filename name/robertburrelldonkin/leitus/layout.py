681dd03f0c2e5ae58420d5b549033f81dfed0a66
Robert Burrell Donkin <robertburrelldonkin@blueyonder.co.uk>
2011-08-25 19:43:02 +0100

    Split out layout

---
+++ name/robertburrelldonkin/leitus/layout.py
@@ -0,0 +1,80 @@
+#
+# Copyright (c) Robert Burrell Donkin 2011
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+#
+# Leitus is a suite of higher level functions for cryptographic drives.
+# The config module contains configuration foo.
+#
+# Robert Burrell Donkin, 2011
+#
+
+import os.path
+import json
+
+from name.robertburrelldonkin.leitus import deep
+
+def load(name):
+    return JsonLoader(name).load()
+    
+class ConfigConstants():
+    USER = 'user'
+    PROFILES = 'profiles'
+    SIZE = 'sizeInMeg'
+    NAME = 'name'
+    
+    def build(self, user, profiles, size, name):
+        return {self.USER:user, self.PROFILES: profiles,
+                    self.SIZE: size, self.NAME:name}
+        
+    def userNameFor(self, configuration):
+        return configuration[self.USER]
+        
+    def userFor(self, configuration):
+        return deep.User(self.userNameFor(configuration))
+        
+    def profilesFor(self, configuration):
+        return configuration[self.PROFILES]
+        
+    def nameFor(self, configuration):
+        return configuration[self.NAME]
+        
+    def sizeFor(self, configuration):
+        return configuration[self.SIZE]
+
+
+
+class StandardLayout():
+    
+    BASE = "drives.d"
+    READ_ONLY = 'r'
+    
+    def open(self, resource):
+        return open(os.path.join(self.BASE, resource), self.READ_ONLY)
+
+class JsonLoader():
+    SUFFIX = ".json"
+    
+    def __init__(self, name):
+        self.resource = name + self.SUFFIX
+        self.layout = StandardLayout()
+        
+    def load(self):
+        return json.load(self.layout.open(self.resource))
