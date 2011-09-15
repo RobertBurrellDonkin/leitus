ade747ee29b0b1504f01a90a750d4ef07249d0d1
Robert Burrell Donkin <robertburrelldonkin@blueyonder.co.uk>
2011-09-15 15:04:10 +0100

    Ready for next release

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ from distutils.core import setup
 
 setup(name='leitus',
       description='Leitus is a suite of higher level functions for cryptographic drives.',
-      version='0.1',
+      version='0.2',
       author='Robert Burrell Donkin',
       author_email='leitus@robertburrelldonkin.name',
       url='http://robertburrelldonkin.name',
